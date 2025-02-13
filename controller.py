9ecdd25d09bfec00d6e91d5061493b0f7ea604fd
godmodevegeta <kshubham1904@gmail.com>
2025-02-13 19:19:55 +0530

    Implement URL shortening functionality in controller.py

--- controller.py
+++ controller.py
@@ -3,6 +3,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+smallToLong = {}
+longToSmall = {}
+
 @app.route("/api/hello/")
 def hello():
     return "<h1>Hello there!</h1>"
@@ -11,8 +14,17 @@ def hello():
 def shorten():
     if request.method == 'POST':
         longURL = request.get_json().get("longURL")
-        return f"found url {longURL}"
+        smallURL = generateSmallURL()
+        longToSmall[longURL] = "http://127.0.0.1:5000/api/redirect/" + smallURL
+        smallToLong[smallURL] = longURL
+        return f"found url {longURL} and generated smallURL {longToSmall[longURL]}\n"
     
     else:
         return "<h1>Please input URL</h1>", 200
 
+# @app.route("/api/redirect/")
+# def redirect():
+
+
+def generateSmallURL():
+    return "smallxyx"
